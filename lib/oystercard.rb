347fcb9d3944aae11247555638391b463f5aaedf
Matthew Drake <matdrake@live.co.uk>
2016-01-26 01:01:12 +0000

    Added an in_journey variable and touch in/out methods to track user journeys. Minimum balance required to touch in

--- lib/oystercard.rb
+++ lib/oystercard.rb
@@ -1,12 +1,14 @@
 class Oystercard
 
-  attr_reader :balance
+  attr_reader :balance, :in_journey
 
   DEFAULT_BALANCE = 0.0
   MAXIMUM_BALANCE = 90.0
+  MINIMUM_BALANCE = 1
 
   def initialize
     @balance = DEFAULT_BALANCE
+    @in_journey = false
   end
 
   def top_up(amount)
@@ -14,8 +16,31 @@ class Oystercard
     @balance += amount
   end
 
+  def deduct(amount)
+    @balance -= amount
+  end
+
+  def in_journey?
+    @in_journey
+  end
+
+  def touch_in
+    raise 'Balance too low' unless minimum_balance?
+    @in_journey = true
+  end
+
+  def touch_out
+    @in_journey = false
+  end
+
+  private
+
   def exceed_maximum?(amount)
     (@balance + amount) > 90
+  end
+
+  def minimum_balance?
+    @balance >= MINIMUM_BALANCE
   end
 
 
--- spec/oystercard_spec.rb
+++ spec/oystercard_spec.rb
@@ -9,7 +9,7 @@ describe Oystercard do
   end
 
   it 'can store the balance' do
-    expect(oystercard.instance_variable_get(:@balance)).to be_a(Float)
+    expect(oystercard.instance_variable_get(:@balance)).to be_a Float
   end
 
   it 'has a top_up method' do
@@ -17,11 +17,44 @@ describe Oystercard do
   end
 
   it 'can top up the balance' do
-    expect{ oystercard.top_up 10 }.to change{ oystercard.balance }.by 10.0
+    expect{oystercard.top_up 10}.to change{oystercard.balance}.by 10.0
   end
 
-  it 'has a maximum balance of 90' do
-    expect{ oystercard.top_up 91 }.to raise_error( 'Over maximum balance' )
+  context 'when balance is loaded' do
+
+    before do
+      oystercard.top_up 90
+    end
+
+    it 'has a maximum balance of 90' do
+      expect{oystercard.top_up 0.01}.to raise_error 'Over maximum balance'
+    end
+
+    it 'can deduct money' do
+      expect{oystercard.deduct 10}.to change{oystercard.balance}.to eq 80
+    end
+
+    it 'can touch_in to set in_journey to true' do
+      expect{oystercard.touch_in}.to change{oystercard.in_journey?}.to eq true
+    end
+
+    it 'can touch_out to set in_journey to false' do
+      oystercard.touch_in
+      expect{oystercard.touch_out}.to change{oystercard.in_journey?}.to eq false
+    end
+
+  end
+
+  it 'has an in_journey variable that is false by default' do
+    expect(oystercard.instance_variable_get(:@in_journey)).to eq false
+  end
+
+  it 'has an in_journey? method that checks in_journey variable' do
+    expect(oystercard.in_journey?).to eq false
+  end
+
+  it 'cannot touch in while under minimum balance' do
+    expect{oystercard.touch_in}.to raise_error 'Balance too low'
   end
 
 end
